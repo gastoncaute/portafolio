bb28b7727e01a13d9a2d2b4fb86b4d816df8caa6
gastoncaute <gastoncaute@gmail.com>
2023-12-01 23:19:18 -0300

    header y estilo en figma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "@/components/Header";
 import Portfolio from "@/components/Portfolio";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <Portfolio />
     </main>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Linkedin</li>
+          <li>GitHub</li>
+          <li>Mail</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
