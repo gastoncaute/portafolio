e760a10ebabe95f7d9a425a9c08990242b9429b5
gastoncaute <gastoncaute@gmail.com>
2023-12-02 00:56:53 -0300

    portfolio schema

--- sanity-project/schemas/portfolio.ts
+++ sanity-project/schemas/portfolio.ts
@@ -4,16 +4,100 @@ export default {
   title: 'Portfolio',
   fields: [
     {
+      name: 'language',
+      title: 'Language',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Spanish', value: 'Spanish'},
+          {title: 'English', value: 'English'},
+        ],
+      },
+      validation: (Rule: any) => Rule.required().error('The "Language" is required')
+    },
+    {
       name: 'title',
-      type: 'array',
       title: 'Title',
-      of: [{type: 'block'}],
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+      validation: (Rule: any) => Rule.required().error('The "Title" is required'),
     },
     {
       name: 'description',
-      type: 'array',
       title: 'Description',
-      of: [{type: 'block'}],
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+      validation: (Rule: any) => Rule.required().error('The "Description" is required'),
+    },
+    {
+      name: 'main_image',
+      title: 'Main Image',
+      type: 'object',
+      fields: [
+        {
+          name: 'image',
+          title: 'Image',
+          type: 'image',
+          validation: (Rule: any) => Rule.required().error('The "Image" is required'),
+        },
+        {
+          name: 'epigraph',
+          type: 'array',
+          title: 'Epigraph',
+          of: [{type: 'block'}],
+          validation: (Rule: any) => Rule.required().error('The "Epigraph" is required'),
+        },
+      ],
+      validation: (Rule: any) => Rule.required().error('The "Main Image" is required'),
+    },
+    {
+      name: 'page_link',
+      title: 'Page Link',
+      type: 'string',
+      validation: (Rule: any) => Rule.required().error('The "Page Link" is required'),
+    },
+    {
+      name: 'second_description',
+      title: 'Second Description',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    },
+    {
+      name: 'image_list',
+      title: 'Image List',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'image',
+              type: 'image',
+              title: 'Image',
+              validation: (Rule: any) => Rule.required().error('The "Image List" is required'),
+            },
+            {
+              name: 'epigraph',
+              type: 'array',
+              title: 'Epigraph',
+              of: [{type: 'block'}],
+              validation: (Rule: any) => Rule.required().error('The "Epigraph" is required'),
+            },
+          ],
+        },
+      ],
     },
   ],
 }
