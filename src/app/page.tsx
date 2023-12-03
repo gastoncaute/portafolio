af779742d4c1ec3cb883f00b5961a080e402ce75
gastoncaute <gastoncaute@gmail.com>
2023-12-03 01:48:07 -0300

    main-borders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import Portfolio from "@/components/Portfolio";
 export default function Home() {
   return (
     <main className="main">
-      <div className="main-border-top"></div>
       <Header />
       <Portfolio />
-      <div className="main-border-bottom"></div>
     </main>
   );
 }
