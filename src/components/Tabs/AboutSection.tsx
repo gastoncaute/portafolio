c8a47481297ff42ec04791064bab5e6b6c2900d0
gastoncaute <gastoncaute@gmail.com>
2023-12-03 20:50:03 -0300

    getAbout

--- src/components/Tabs/AboutSection.tsx
+++ src/components/Tabs/AboutSection.tsx
@@ -1,4 +1,37 @@
 import React from "react";
+// import { client } from "@/utils/configSanity";
+// import { About } from "@/types/components.types";
+// import { PortableText } from "@portabletext/react";
+
+// async function getAboutData() {
+//   const query = `*[_type == 'about']`;
+//   const data = await client.fetch(query);
+//   return data as About[];
+// }
+
+// const aboutSection = async ({
+//   selectedLanguage,
+// }: {
+//   selectedLanguage: any;
+// }) => {
+//   const data = (await getAboutData()) as About[];
+//   return (
+//     <section>
+//       {data?.map((item) => (
+//         <section key={item._id}>
+//           <h1>
+//             <PortableText value={item.description} />
+//           </h1>
+//           <h2>
+//             <PortableText value={item.education} />
+//           </h2>
+//         </section>
+//       ))}
+//     </section>
+//   );
+// };
+
+// export default aboutSection;
 
 export default function AboutSection({
   selectedLanguage,
