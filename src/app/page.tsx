8dec842cada52f3387e120cccf44af6e77c55fdb
gastoncaute <gastoncaute@gmail.com>
2023-12-03 01:38:34 -0300

    borders styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import Portfolio from "@/components/Portfolio";
 export default function Home() {
   return (
     <main className="main">
+      <div className="main-border-top"></div>
       <Header />
       <Portfolio />
+      <div className="main-border-bottom"></div>
     </main>
   );
 }
