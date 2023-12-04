cebbae52a23b210831011c313d11e0b6aa5fae86
gastoncaute <gastoncaute@gmail.com>
2023-12-04 00:25:26 -0300

    getAbout

--- src/components/Tabs/AboutSection.tsx
+++ src/components/Tabs/AboutSection.tsx
@@ -1,47 +1,58 @@
-import React from "react";
-// import { client } from "@/utils/configSanity";
-// import { About } from "@/types/components.types";
-// import { PortableText } from "@portabletext/react";
-
-// async function getAboutData() {
-//   const query = `*[_type == 'about']`;
-//   const data = await client.fetch(query);
-//   return data as About[];
-// }
-
-// const aboutSection = async ({
-//   selectedLanguage,
-// }: {
-//   selectedLanguage: any;
-// }) => {
-//   const data = (await getAboutData()) as About[];
-//   return (
-//     <section>
-//       {data?.map((item) => (
-//         <section key={item._id}>
-//           <h1>
-//             <PortableText value={item.description} />
-//           </h1>
-//           <h2>
-//             <PortableText value={item.education} />
-//           </h2>
-//         </section>
-//       ))}
-//     </section>
-//   );
-// };
-
-// export default aboutSection;
+"use client";
+import { About } from "@/types/components.types";
+import { PortableText } from "@portabletext/react";
+import React, { useState, useEffect } from "react";
 
 export default function AboutSection({
   selectedLanguage,
 }: {
   selectedLanguage: any;
 }) {
+  const [aboutData, setAboutData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://ouy7dg4a.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27about%27%5D"
+        );
+
+        if (!response.ok) {
+          throw new Error("Error fetching data");
+        }
+
+        const data = await response.json();
+        setAboutData(data.result); // Assuming there is only one result
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section>
       <h1>About Section</h1>
-      <h2>{selectedLanguage}</h2>
+      {aboutData && (
+        <>
+          <h2>{selectedLanguage}</h2>
+          {aboutData.map((item: About) => (
+            <section key={item._id}>
+              <h1>{item.lenguage}</h1>
+              <h1>
+                <PortableText value={item.description} />
+              </h1>
+              <h2>
+                <PortableText value={item.skills} />
+              </h2>
+              <h3>
+                <PortableText value={item.education} />
+              </h3>
+            </section>
+          ))}
+        </>
+      )}
     </section>
   );
 }
