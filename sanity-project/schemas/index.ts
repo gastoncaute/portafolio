0a8674fb7c80daca4d691f7d302d1fa1a1f9c212
gastoncaute <gastoncaute@gmail.com>
2023-12-05 11:42:28 -0300

    sanity schemas

--- sanity-project/schemas/index.ts
+++ sanity-project/schemas/index.ts
@@ -1,5 +1,4 @@
 import about from './about'
-import contact from './contact'
 import project from './project'
 
-export const schemaTypes = [project, about, contact]
+export const schemaTypes = [project, about]
